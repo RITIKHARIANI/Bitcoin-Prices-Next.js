390e3f1e8dced881b7df07061fe349aad658ce5e
RITIKHARIANI <ritikhariani@gmail.com>
2020-07-02 02:18:27 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Bitcoin Price
+
+Next.js app that displays the current Bitcoin value in USD, GBP and EUR
+
+## Quick Start
+
+```
+# Install dependencies
+npm install
+
+# Serve on localhost:3000
+npm run dev
+
+# Build for production
+npm run build
+```
---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Bitcoin Prices</h1>
+      <p>Application to view Bitcoin prices in different currencies</p>
+    </div>
+  </Layout>
+);
+
+export default About;
